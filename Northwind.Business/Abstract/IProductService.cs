ac24605d5ce2aa1b167402a3f1be72dbc3cd8759
MustafaCEVIK <cevikksoft@gmail.com>
2020-01-21 01:16:03 +0300

    Generate business layer for Product class (dependency injection).

---
+++ Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(int productID);
+        IList<Product> GetList();
+        IList<Product> GetListByCategoryId(int categoryID);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,50 @@
+﻿using Northwind.Business.Abstract;
+using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public Product GetById(int productID)
+        {
+            return _productDal.Get(p => p.ProductID == productID);
+        }
+
+        public IList<Product> GetList()
+        {
+            return _productDal.GetList().ToList();
+        }
+
+        public IList<Product> GetListByCategoryId(int categoryID)
+        {
+            return _productDal.GetList(p => p.CategoryID == categoryID);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
