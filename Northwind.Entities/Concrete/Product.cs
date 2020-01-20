9e1c29639b56b17ca387f82ed85cf0d34a0fd724
MustafaCEVIK <cevikksoft@gmail.com>
2020-01-20 21:50:19 +0300

    Generate entity layer for Product class.

---
+++ Northwind.Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Northwind.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitInStock { get; set; }
+    }
+}
