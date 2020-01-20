b55a3b443ceb3a9aa19bb6f09848e24948ce2ef4
MustafaCEVIK <cevikksoft@gmail.com>
2020-01-21 00:22:56 +0300

    Generate data access layer for Product class (repository pattern, context, repository base).

---
+++ Northwind.DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=SAMSUNG\SQLEXPRESS;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,14 @@
+﻿using Northwind.Core.DataAccess.EntityFramework;
+using Northwind.DataAccess.Abstract;
+using Northwind.DataAccess.Concrete.EntityFramework.Contexts;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+    }
+}
