a9da2cdea0bf0ae5fcef0e82c5bbb79eb25e70d1
MustafaCEVIK <cevikksoft@gmail.com>
2020-01-25 17:44:48 +0300

    Add validation rules for product class (with fluent validation).

---
+++ Northwind.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).Length(5, 40);
+
+            RuleFor(p => p.CategoryID).NotEmpty();
+
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(1);
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryID == 1);
+
+            RuleFor(p => p.ProductName).Must(StartsWithPr);
+        }
+
+        private bool StartsWithPr(string arg)
+        {
+            return arg.StartsWith("Pr");
+        }
+    }
+}
