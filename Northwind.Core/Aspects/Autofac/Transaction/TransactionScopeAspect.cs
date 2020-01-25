e31978198543bb9e715c74d7064f8138f2ecedd2
MustafaCEVIK <cevikksoft@gmail.com>
2020-01-26 01:38:13 +0300

    Generate transaction scope aspect.

---
+++ Northwind.Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using Northwind.Core.Utilities.Interceptors.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Northwind.Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (Exception e)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
